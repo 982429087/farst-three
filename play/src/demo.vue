1ad69c6eb1eebf046dcf01e55039398fd5246a81
wyy <982429087@qq.com>
2023-06-27 09:09:05 +0800

    feat: 上传调试代码

---
+++ play/src/demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="farst-three">
+    <ft-scene>
+      <FtPerspectiveCamera>
+        <FtWebglRenderer />
+      </FtPerspectiveCamera>
+    </ft-scene>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.farst-three {
+  height: 100vh;
+  width: 100vw;
+}
+
+
+</style>
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
