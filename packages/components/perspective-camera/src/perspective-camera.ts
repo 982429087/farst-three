f50a32e8b1a6010247f0e14c8cdc669fe288665f
wyypc <982429087@qq.com>
2023-06-28 22:36:02 +0800

    feat: 修改 perspective-camera

--- packages/components/perspective-camera/src/perspective-camera.ts
+++ packages/components/perspective-camera/src/perspective-camera.ts
@@ -1,13 +1,34 @@
 import { buildProps } from '@farst-three/utils'
+import type { PerspectiveCamera } from 'three'
 
 import type { ExtractPropTypes } from 'vue'
 import type PerspectiveCameraComponent from './perspective-camera.vue'
 
-export const perspectiveCameraProps = buildProps({})
+export type PerspectiveCameraParams = ConstructorParameters<
+  typeof PerspectiveCamera
+>
 
+export const perspectiveCameraProps = buildProps({
+  fov: {
+    type: Number,
+  },
+  aspect: {
+    type: Number,
+  },
+  near: {
+    type: Number,
+  },
+  far: {
+    type: Number,
+  },
+})
+export const perspectiveCameraEmits = {
+  load: (camera: PerspectiveCamera) => camera,
+}
 export type PerspectiveCameraProps = ExtractPropTypes<
   typeof perspectiveCameraProps
 >
 export type PerspectiveCameraInstance = InstanceType<
   typeof PerspectiveCameraComponent
 >
+export type PerspectiveCameraEmits = typeof perspectiveCameraEmits
--- packages/components/perspective-camera/src/perspective-camera.vue
+++ packages/components/perspective-camera/src/perspective-camera.vue
@@ -3,25 +3,48 @@
 </template>
 
 <script lang="ts" setup>
-import { shallowRef } from 'vue'
+import { onBeforeUnmount, shallowRef } from 'vue'
 import { provide } from '@vue/runtime-core'
 import { PerspectiveCamera } from 'three'
-import { cameraInjectionKey } from '@farst-three/constants/injection'
-import { perspectiveCameraProps } from './perspective-camera'
+import { debounce } from 'lodash-es'
+import {
+  perspectiveCameraInjectionKey,
+  sceneRefInjectionKey,
+} from '@farst-three/constants/injection'
+import { useInjection } from '@farst-three/hooks'
+import {
+  perspectiveCameraEmits,
+  perspectiveCameraProps,
+} from './perspective-camera'
 
 defineOptions({
   name: 'FtPerspectiveCamera',
 })
 
-defineProps(perspectiveCameraProps)
+const props = defineProps(perspectiveCameraProps)
+const emit = defineEmits(perspectiveCameraEmits)
 
 // init here
+const container = useInjection<HTMLDivElement>(sceneRefInjectionKey)
+
 const camera = new PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+  props.fov,
+  props.aspect || container.offsetWidth / container.offsetHeight,
+  props.near,
+  props.far
 )
-camera.position.z = 5
-provide(cameraInjectionKey, shallowRef(camera))
+
+emit('load', camera)
+
+provide(perspectiveCameraInjectionKey, shallowRef(camera))
+const resize = () => {
+  camera.aspect = container.offsetWidth / container.offsetHeight
+  camera.updateProjectionMatrix()
+}
+const dOb = debounce(() => resize(), 10)
+const observer = new ResizeObserver(dOb)
+observer.observe(container)
+onBeforeUnmount(() => {
+  observer.unobserve(container)
+})
 </script>
