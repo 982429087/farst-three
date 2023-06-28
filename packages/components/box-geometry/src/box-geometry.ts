4bb9cd4928fd1b3179fe8c8feb58c762bebfb8ee
wyypc <982429087@qq.com>
2023-06-28 22:34:50 +0800

    feat: 修改box-geometry

--- packages/components/box-geometry/src/box-geometry.ts
+++ packages/components/box-geometry/src/box-geometry.ts
@@ -1,9 +1,21 @@
 import { buildProps } from '@farst-three/utils'
-
+import type { BoxGeometry } from 'three'
 import type { ExtractPropTypes } from 'vue'
-import type BoxGeometry from './box-geometry.vue'
+import type BoxGeometryComponent from './box-geometry.vue'
 
-export const boxGeometryProps = buildProps({})
+export const boxGeometryProps = buildProps({
+  width: Number,
+  height: Number,
+  depth: Number,
+  widthSegments: Number,
+  heightSegments: Number,
+  depthSegments: Number,
+})
+
+export const boxGeometryEmits = {
+  load: (geometry: BoxGeometry) => geometry,
+}
 
 export type BoxGeometryProps = ExtractPropTypes<typeof boxGeometryProps>
-export type BoxGeometryInstance = InstanceType<typeof BoxGeometry>
+export type BoxGeometryInstance = InstanceType<typeof BoxGeometryComponent>
+export type BoxGeometryEmits = typeof boxGeometryEmits
--- packages/components/box-geometry/src/box-geometry.vue
+++ packages/components/box-geometry/src/box-geometry.vue
@@ -6,16 +6,25 @@
 import { BoxGeometry } from 'three'
 import { useInjection } from '@farst-three/hooks'
 import { meshInjectionKey } from '@farst-three/constants/injection'
-import { boxGeometryProps } from './box-geometry'
+import { boxGeometryEmits, boxGeometryProps } from './box-geometry'
 import type { Mesh } from 'three'
 defineOptions({
   name: 'FtBoxGeometry',
 })
 
-defineProps(boxGeometryProps)
+const props = defineProps(boxGeometryProps)
+const emit = defineEmits(boxGeometryEmits)
 
 // init here
-const geometry = new BoxGeometry(1, 1, 1)
+const geometry = new BoxGeometry(
+  props.width,
+  props.height,
+  props.depth,
+  props.widthSegments,
+  props.heightSegments,
+  props.depthSegments
+)
+emit('load', geometry)
 const mesh = useInjection<Mesh>(meshInjectionKey)
 mesh.geometry = geometry
 </script>
