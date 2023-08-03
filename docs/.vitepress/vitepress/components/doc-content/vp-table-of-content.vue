e30681ede6488c62749ceb137617f9de1f40c66e
wyy <982429087@qq.com>
2023-08-03 17:48:50 +0800

    docs: 修改文档

--- docs/.vitepress/vitepress/components/doc-content/vp-table-of-content.vue
+++ docs/.vitepress/vitepress/components/doc-content/vp-table-of-content.vue
@@ -6,9 +6,9 @@ import { useActiveSidebarLinks } from '../../composables/active-bar'
 
 import sponsorLocale from '../../../i18n/component/sponsor.json'
 import { useLang } from '../../composables/lang'
-import SponsorsButton from '../sponsors/sponsors-button.vue'
-import SponsorRightTextList from '../sponsors/right-richtext-list.vue'
-import SponsorRightLogoSmallList from '../sponsors/right-logo-small-list.vue'
+// import SponsorsButton from '../sponsors/sponsors-button.vue'
+// import SponsorRightTextList from '../sponsors/right-richtext-list.vue'
+// import SponsorRightLogoSmallList from '../sponsors/right-logo-small-list.vue'
 import tag from '../../../plugins/tag'
 // import SponsorLarge from '../vp-sponsor-large.vue'
 
@@ -58,12 +58,12 @@ const sponsor = computed(() => sponsorLocale[lang.value])
         class="mt-8 toc-ads flex flex-col"
         item-style="width: 180px; height: 55px;"
       /> -->
-      <p class="text-14px font-300 color-$text-color-secondary">
+      <!-- <p class="text-14px font-300 color-$text-color-secondary">
         {{ sponsor.sponsoredBy }}
-      </p>
-      <sponsors-button class="sponsors-button mt-4 w-100%" />
+      </p> -->
+      <!-- <sponsors-button class="sponsors-button mt-4 w-100%" />
       <sponsor-right-logo-small-list />
-      <sponsor-right-text-list />
+      <sponsor-right-text-list /> -->
     </nav>
   </aside>
 </template>
