1d2a085d5f230dca536ccb049c394a029aa36162
wyy <982429087@qq.com>
2023-11-20 16:00:11 +0800

    refactor: 删除多余组件

--- packages/components/components/geo-plane/index.ts
+++
@@ -1,7 +0,0 @@
-import { withInstall } from '@farst-three/utils'
-import GeoPlane from './src/geo-plane.vue'
-
-export const FtGeoPlane = withInstall(GeoPlane)
-export default FtGeoPlane
-
-export * from './src/geo-plane'
--- packages/components/components/index.ts
+++ packages/components/components/index.ts
@@ -12,5 +12,5 @@ export * from './point-marker'
 export * from './pillar'
 export * from './edge-move-line'
 export * from './high-light'
-export * from './geo-plane'
+
 export * from './outline'
