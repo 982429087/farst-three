71944821789faaf83b0067d8119c04c66105670f
wyy <982429087@qq.com>
2023-12-07 14:42:10 +0800

    feat: 调整代码

--- packages/hooks/components/index.ts
+++ packages/hooks/components/index.ts
@@ -1,3 +1,3 @@
-export * from './FtObject/index'
-export * from './Pass/index'
-export * from './GeoJsonPlane/index'
+export * from './FtObject'
+export * from './Pass'
+export * from './GeoJsonPlane'
