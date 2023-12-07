f68d182b97288e562fcac57d1f836fd7957eabb7
982429087 <982429087@qq.com>
2023-12-07 09:51:53 +0000

    website deploy

--- assets/chunks/index.4875a908.js
+++
@@ -1,1 +0,0 @@
-import{A as a,d as t,aM as l,z as p,H as u,J as c}from"../app.e23ab445.js";import{b as d}from"./index.4766a5d6.js";const i=a({size:Number}),m=t({name:"FtAxesHelper"}),f=t({...m,props:i,setup(n){const o=n;let s=d(),e=new l(o.size);return s.add(e),p(()=>{e==null||e.dispose(),e=null,s=null}),(r,x)=>u(r.$slots,"default")}});var _=f;const b=c(_);export{b as F};
---
+++ assets/chunks/index.c2cef43c.js
@@ -0,0 +1,1 @@
+import{A as a,d as t,aM as l,z as p,H as u,J as c}from"../app.5a7ff449.js";import{b as d}from"./index.dbd843fe.js";const i=a({size:Number}),m=t({name:"FtAxesHelper"}),f=t({...m,props:i,setup(n){const o=n;let s=d(),e=new l(o.size);return s.add(e),p(()=>{e==null||e.dispose(),e=null,s=null}),(r,x)=>u(r.$slots,"default")}});var _=f;const b=c(_);export{b as F};
